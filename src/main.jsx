11182ec4b2fb498e67d9dbfe8414a6462975a953
alejf1 <123578439+alejf1@users.noreply.github.com>
2025-09-26 14:25:29 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Tailwind output
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import AuthGate from './components/AuthGate.jsx';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>
 );
